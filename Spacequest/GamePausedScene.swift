8b6a803b2feeb164047771d9734562e6725139a9
Rafal Sroka <rafal.sroka.it@gmail.com>
2014-08-06 11:58:05 +0200

    Place buttons on the screen.

--- Spacequest/GamePausedScene.swift
+++ Spacequest/GamePausedScene.swift
@@ -28,23 +28,36 @@ class GamePausedScene: SKScene
         resumeButton!.title = NSLocalizedString("resume", comment: "")
         
         // Restart button.
-        
-        buttons = [resumeButton!, restartButton!]
-        resumeButton = Button(
+        restartButton = Button(
             normalImageNamed: "menu_button_normal",
             selectedImageNamed: "menu_button_normal")
-        resumeButton!.position = CGPoint(
-            x: CGRectGetWidth(self.frame)/2,
-            y: CGRectGetHeight(self.frame)/2)
-        resumeButton!.title = NSLocalizedString("resume", comment: "")
+
+        restartButton!.title = NSLocalizedString("restart", comment: "")
         
-        for (index, button) in enumerate(buttons)
+        buttons = [resumeButton!, restartButton!]
+        let verticalPadding = 20.0
+        var totalButtonsHeight = 0.0
+        
+        // Calculate total height of the buttons area.
+        for (index, button) in enumerate(buttons!)
         {
-            button!.position = CGPoint(
+            totalButtonsHeight += button.size.height
+            totalButtonsHeight += index != buttons!.count - 1 ? verticalPadding : 0.0
+        }
+        
+        // Calculate origin of first button.
+        var buttonOriginY = CGRectGetHeight(self.frame) / 2 + totalButtonsHeight / 2
+
+        // Place buttons in the scene.
+        for (index, button) in enumerate(buttons!)
+        {
+            button.position = CGPoint(
                 x: CGRectGetWidth(self.frame)/2,
-                y: CGRectGetHeight(self.frame)/2)
+                y: buttonOriginY - button.size.height/2)
             
             self.addChild(button)
+            
+            buttonOriginY -= button.size.height + verticalPadding
         }
     }
 }
