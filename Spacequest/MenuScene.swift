e0b92c0a7c47a5fdb206bee4b65ff67189e82709
Rafal Sroka <rafal.sroka@srf.com>
2026-02-28 15:58:38 +0100

    Refactor MenuScene

--- Spacequest/MenuScene.swift
+++ Spacequest/MenuScene.swift
@@ -17,8 +17,6 @@ import SpriteKit
 
 class MenuScene: SKScene {
 
-    private var background: SKSpriteNode?
-
     // MARK: - Scene lifecycle
 
     override func sceneDidLoad() {
@@ -30,11 +28,11 @@ class MenuScene: SKScene {
     // MARK: - Configuration
 
     private func configureBackground() {
-        background = SKSpriteNode(imageNamed: ImageName.MenuBackgroundPhone.rawValue)
-        background!.size = size
-        background!.position = CGPoint(x: size.width/2,
-                                       y: size.height/2)
-        background!.zPosition = -1000
-        addChild(background!)
+        let background = SKSpriteNode(imageNamed: ImageName.MenuBackgroundPhone.rawValue)
+        background.size = size
+        background.position = CGPoint(x: size.width/2,
+                                      y: size.height/2)
+        background.zPosition = -1000
+        addChild(background)
     }
 }
